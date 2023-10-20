29e69874a76f971ffeaa81b3c226d11ab4cf878d
Riyan042 <riyandarmawan042@gmail.com>
2023-10-20 10:47:49 +0700

    pertemuan 6

---
+++ pertemuan6/inheritance.php
@@ -0,0 +1,59 @@
+<?php
+
+class Produk {
+    public $judul,
+           $penulis,
+           $penerbit,
+           $harga, 
+           $jumlahHalaman, 
+           $waktuMain;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga  = 0, $jumlahHalaman = 0, $waktuMain = 0) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        $this->jumlahHalaman = $jumlahHalaman;
+        $this->waktuMain = $waktuMain;
+    }
+
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoProduk() {
+        // Komik : NAruto | Masashi Kishimoto, Shonen Jump (Rp. 30000) - 100 Halaman.
+        // Game : Uncharted | Neil Druckmann, Sony Computer (Rp. 250000) ~ 50 Jam.
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+
+        return $str;
+    }
+}
+
+class Komik extends Produk {
+    public function getInfoProduk(){
+        $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jumlahHalaman} Halaman.";
+        return $str;
+    }
+}
+
+class Gane extends Produk {
+    public function getInfoProduk(){
+        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ~ {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+class CetakInfoProduk {
+    public function cetak(Produk $produk) {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0);
+$produk2 = new Gane("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50);
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
